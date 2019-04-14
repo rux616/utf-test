e4ce21cd9d4cc2a1307cf3d11426ff69380c9c10
rux616 <5643061+rux616@users.noreply.github.com>
2019-04-14 00:07:16 -0600

    Initial script

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "begin test"
+echo "----------"
+echo "block symbols: █ ▇ ▆ ▅ ▄ ▃ ▂ ▁"
+echo "plug symbol: 🔌"
+echo "battery symbol: 🔋"
+echo "warning symbol: ⚠️"
+echo "moon symbols: 🌕 🌖 🌗 🌘 🌑"
+echo "lightning bold symbol: ⚡"
+echo "thumbs down symbol: 👎"
+echo "--------"
+echo "end test"
+
